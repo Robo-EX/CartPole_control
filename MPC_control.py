df121f9a914764688c37157e5514580fcac02445
Bhavik Mukesh Panchal <bmp7963@nyu.edu>
2022-02-21 13:46:21 -0500

    update

--- MPC_control.py
+++ MPC_control.py
@@ -13,10 +13,6 @@ import numpy as np
 
 # Model parameters
 
-l_bar = 2.0  # length of bar
-# M = 1.0  # [kg]
-# m = 0.3  # [kg]
-# g = -9.8  # [m/s^2]
 
 nx = 4  # number of state
 nu = 1  # number of input
@@ -83,19 +79,6 @@ def main():
             planeId, 1, p.POSITION_CONTROL, targetPosition=x[2])
         p.stepSimulation()
         time.sleep(1./240.)
-
-    #     if show_animation:
-    #         plt.clf()
-    #         px = float(x[0])
-    #         theta = float(x[2])
-    #         plot_cart(px, theta)
-
-    #         plt.pause(0.001)
-
-    # print("Finish")
-    # print(f"x={float(x[0]):.2f} [m] , theta={math.degrees(x[2]):.2f} [deg]")
-    # if show_animation:
-    #     plt.show()
 
 
 def simulation(x, u):
@@ -172,50 +155,6 @@ def get_model_matrix():
 
 def flatten(a):
     return np.array(a).flatten()
-
-
-# def plot_cart(xt, theta):
-#     cart_w = 1.0
-#     cart_h = 0.5
-#     radius = 0.1
-
-#     cx = np.array([-cart_w / 2.0, cart_w / 2.0, cart_w /
-#                    2.0, -cart_w / 2.0, -cart_w / 2.0])
-#     cy = np.array([0.0, 0.0, cart_h, cart_h, 0.0])
-#     cy += radius * 2.0
-
-#     cx = cx + xt
-
-#     bx = np.array([0.0, l_bar * math.sin(-theta)])
-#     bx += xt
-#     by = np.array([cart_h, l_bar * math.cos(-theta) + cart_h])
-#     by += radius * 2.0
-
-#     angles = np.arange(0.0, math.pi * 2.0, math.radians(3.0))
-#     ox = np.array([radius * math.cos(a) for a in angles])
-#     oy = np.array([radius * math.sin(a) for a in angles])
-
-#     rwx = np.copy(ox) + cart_w / 4.0 + xt
-#     rwy = np.copy(oy) + radius
-#     lwx = np.copy(ox) - cart_w / 4.0 + xt
-#     lwy = np.copy(oy) + radius
-
-#     wx = np.copy(ox) + bx[-1]
-#     wy = np.copy(oy) + by[-1]
-
-#     plt.plot(flatten(cx), flatten(cy), "-b")
-#     plt.plot(flatten(bx), flatten(by), "-k")
-#     plt.plot(flatten(rwx), flatten(rwy), "-k")
-#     plt.plot(flatten(lwx), flatten(lwy), "-k")
-#     plt.plot(flatten(wx), flatten(wy), "-k")
-#     plt.title(f"x: {xt:.2f} , theta: {math.degrees(theta):.2f}")
-
-#     # for stopping simulation with the esc key.
-#     plt.gcf().canvas.mpl_connect(
-#         'key_release_event',
-#         lambda event: [exit(0) if event.key == 'escape' else None])
-
-#     plt.axis("equal")
 
 
 if __name__ == '__main__':
